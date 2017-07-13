64a736030eee34de8b9eec7ac28e11bf2e7eb336
cuddrow <hallvard.heiberg@gmail.com>
2017-07-13 16:00:18 +0200

    Ready to push

--- arraytolist.py
+++ arraytolist.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 mylist = []
 for i in range(20):
-    a = random.randint(, 100)
+    a = random.randint(1, 100)
     mylist.append(a)
 
 print (mylist)
@@ -11,9 +11,9 @@ print (len(mylist))
 np.savetxt('randomlist.csv', mylist, delimiter=',')
 
 loadedlist = np.loadtxt('randomlist.csv', delimiter=',')
-print ("Contents of loadedlist: " + str(loadedlist))
+print ("Contents of loadedlist as array object: " + str(loadedlist))
 
 #print(type(loadedlist))
 x = np.array(loadedlist).tolist()
 #print(type(loadedlist))
-print(str(x))
+print("Contents of loadedlist as list object: " + str(x))
